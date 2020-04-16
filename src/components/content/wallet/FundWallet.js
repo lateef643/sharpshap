91ccb21aa7a7a568733e71731d04d32866fd2e42
Tochukwu Nwanguma <Tochukwu@cico.ng>
2020-04-16 10:56:52 +0100

    Create fund wallet status page Edit fund wallet request page

--- src/components/content/wallet/FundWallet.js
+++ src/components/content/wallet/FundWallet.js
@@ -1,35 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { connect } from "react-redux";
-import style from './FundWallet.module.scss';
+import Loader from "../../partials/Loader";
 import { setCurrentPage } from "../../../actions/page";
+import FundWalletStatus from "./FundWalletStatus";
+import style from './FundWallet.module.scss';
 
 export const FundWallet = ({ changeCurrentPage }) => {
-  changeCurrentPage({
-    heading: "Fund Wallet",
-    search: false
-  });
+  useEffect(() => {
+    changeCurrentPage({
+      heading: "Fund Wallet",
+      search: false
+    });
+  }, [changeCurrentPage]);
 
-  const [agentName, setAgentName] = useState("");
-  const [walletNumber, setWalletNumber] = useState("");
+  const [bankDetails, setBankDetails] = useState("");
+  const [tellerNumber, setTellerNumber] = useState("");
   const [amount, setAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [requestStatus, setRequestStatus] = useState(false);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
     console.log({
-      agentName,
-      walletNumber,
+      bankDetails,
+      tellerNumber,
       amount
-    })
+    });
+
+    const payload = {
+      bank: bankDetails.bank,
+      accountNumber: bankDetails.accountNumber,
+      tellerNumber,
+      amount
+    }
+
+    axios.post('', payload)
+      .then(res => {
+        console.log(res);
+        setLoading(false);
+        setRequestStatus(true);
+      })
+      .catch(err => {
+        console.log(err);
+        setLoading(false);
+      })
   };
 
-  const handleAgentNameChange = (e) => {
-    const newAgentName = e.target.value;
-    setAgentName(newAgentName);
+  const handleBankDetailsChange = (e) => {
+    const newBankDetails = JSON.parse(e.target.value);
+    setBankDetails(newBankDetails);
   };
 
-  const handleWalletNumberChange = (e) => {
-    const newWalletNumber = e.target.value;
-    setWalletNumber(newWalletNumber);
+  const handleTellerNumberChange = (e) => {
+    const newTellerNumber = e.target.value;
+    setTellerNumber(newTellerNumber);
   }
 
   const handleAmountChange = (e) => {
@@ -38,22 +64,28 @@ export const FundWallet = ({ changeCurrentPage }) => {
   }
 
   return (
-  <div className={style.container}>
-    <form className={style.form} onSubmit={handleOnSubmit} >
-      <label>
-        <span>Agent Name</span>
-        <input type="text" onChange={handleAgentNameChange} />      
-      </label>
-      <label>
-        <span>Wallet Number</span>
-        <input type="text" onChange={handleWalletNumberChange} />      
-      </label>    
-      <label>
-        <span>Amount</span>
-        <input type="text" onChange={handleAmountChange} />      
-      </label>        
-      <button type="submit">Submit</button>
-    </form>    
+    <div className={style.container}>{
+      requestStatus ? <FundWalletStatus /> :
+      <form className={style.form} onSubmit={handleOnSubmit} >
+        <label>
+          <span>Amount</span>
+          <input type="text" onChange={handleAmountChange} />      
+        </label> 
+        <label>
+          <span>Bank</span>
+          <select onChange={handleBankDetailsChange}>   
+            <option value="">Select Bank</option>
+            <option value={JSON.stringify({bank: "Suntrust bank", accountNumber: "0012345678" })}>Suntrust Bank  0012345678 CicoServe Payment</option>
+            <option value={JSON.stringify({bank: "Access bank", accountNumber: "0012345678" })}>Access Bank 0012345678 CicoServe Payment</option>
+            <option value={JSON.stringify({bank: "Fidelity bank", accountNumber: "0012345678" })}>Fidelity Bank 0012345678 CicoServe Payment</option>
+          </select>
+        </label>
+        <label>
+          <span>Teller Number</span>
+          <input type="text" onChange={handleTellerNumberChange} />
+        </label>    
+        <button type="submit">{loading ? <Loader size="small" color="white" position="center" /> : "Submit" }</button>
+    </form>}  
   </div>
 )}
 
---
+++ src/components/content/wallet/FundWalletStatus.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import check from "../../../assets/images/check.svg";
+// import cross from "../../../../assets/images/redCross.svg";
+import styles from './FundWalletStatus.module.scss';
+
+export const FundWalletStatus = ({ transactionStatus, transactionCost, amount,
+successPayload}) => { 
+  return (
+    <div className={styles.container}>
+      <div className={styles.sectionContainer} >
+        <div className={styles.imageContainer}>
+          <img src={check} alt="checkmark" />
+          <p>Transaction Successful</p>
+        </div>
+        <div className={styles.contentContainer}>
+          <div className={styles.content}>
+            <p>Your wallet funding request has been recieved and our representative will 
+              validate your request shortly.</p>  
+            <div><Link to="/">&larr; Home</Link></div>
+          </div>
+        </div>
+      </div>    
+    </div>
+)};
+
+export default FundWalletStatus;
