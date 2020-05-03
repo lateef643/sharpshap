2c68add55e9857a684b1ffb3609bca0becbe3248
Tochukwu Nwanguma <Tochukwu@cico.ng>
2020-05-03 15:49:09 +0100

    Add filters to transaction log Edit date picker styles Edit pagination

--- src/components/content/transactions/TransactionLog.js
+++ src/components/content/transactions/TransactionLog.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState } from "react";
 import axios from "axios";
 import ListLoader from "../../partials/ListLoader";
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import style from './TransactionLog.module.scss';
+import styles from './TransactionLog.module.scss';
 import { setCurrentPage } from "../../../actions/page";
 import { setTransactionLog } from "../../../actions/transaction";
 import { AGENT_TRANSACTION_HISTORY } from "../../../store/api/constants";
@@ -17,10 +18,52 @@ export const TransactionLog = ({ changeCurrentPage, setTransactionsLog, uuid }) 
   const [pageNumbers, setPageNumbers] = useState([]);
   const [lastPage, setLastPage] = useState("");
   const [pageChangeLoading, setPageChangeLoading] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [filter, setFilter] = useState("");
   const firstPage = 1;
 
-  //transactions
-  console.log(currentPage)
+  useEffect(() => {
+    if (typeof filter === "number") {
+      const payload = {
+        "transaction_type_id": 2
+      }
+
+      console.log('this is called')
+      console.log(filter)
+      axios.get('https://api.cico.ng/api/transactions/filter', payload)
+      .then(res => {
+        const transactions = res.data.data.data;
+        const total = res.data.data.total;
+        const perPage = res.data.data.per_page;
+        const lastPage = res.data.data.last_page;
+        let pageNumbers = [];
+        console.log("filtered", res.data.data)
+        console.log(res)
+  
+        if (total !== null && total > 0) {
+          for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+            pageNumbers.push(i);
+          };
+          setPageNumbers(pageNumbers);
+        }
+        // const businessName = res.data.data.agent.business_name;
+        // const transactions = res.data.data.transaction;
+        // const walletInfo = res.data.data.wallet;
+        // setWalletInfo(walletInfo);
+        setLoading(false);
+        setLastPage(lastPage)
+        // setBusinessName(businessName);
+        setTransactions(transactions);
+        setPageChangeLoading(false);
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.response) {
+          console.log(err.response.data)
+        }
+      });
+    }
+  }, [filter]);
 
   useEffect(() => {
     axios.get(`${AGENT_TRANSACTION_HISTORY}?page=${currentPage}`)
@@ -64,59 +107,92 @@ export const TransactionLog = ({ changeCurrentPage, setTransactionsLog, uuid }) 
     });    
   }, [changeCurrentPage]);
 
+
+  const handleFilterChange = (e) => {
+    console.log('this bitch is running')
+    const filter = parseInt(e.target.value);
+    setFilter(filter);
+  }
+
   return (
-    <div className={style.container}>
-      {loading || pageChangeLoading ? <div className={style.loaderContainer}><ListLoader /></div> : undefined}
+    <div className={styles.container}>
+    {!loading && transactions.length > 0 ?
+      <div className={styles.filters}>
+        <div className={styles.dateFilterContainer}>
+          <div className={styles.dateFilter}>
+            <DateRangePicker
+              onChange={date => {
+                console.log(date);
+                setDate(date);
+              }}
+              value={date}
+            />
+          </div>
+        </div>
+        <div>
+          <select onChange={handleFilterChange}>
+            <option value="">Filter by transaction type</option>
+            <option value="">All transactions</option>
+            <option value="2">Transfer</option>
+            <option value="3">Airtime</option>
+          </select>          
+        </div>
+      </div> :
+      undefined}
+      {loading || pageChangeLoading ? <div className={styles.loaderContainer}><ListLoader /></div> : undefined}
       {!loading && transactions.length > 0 ? 
-        <div className={style.heading}>
-          <span className={style.itemOne}>Status</span>
-          <span className={style.itemTwo}>Amount</span>
-          <span className={style.itemThree}>Reference</span>
-          <span className={style.itemFour}>Type</span>
-          <span className={style.itemFive}>Customer</span>
-          <span className={style.itemSix}>Agent</span>
-          <span className={style.itemSeven}>Date Created</span>
-          <span className={style.itemEight}>Details</span>
+        <div className={styles.heading}>
+          <span className={styles.itemOne}>Status</span>
+          <span className={styles.itemTwo}>Amount</span>
+          <span className={styles.itemThree}>Reference</span>
+          <span className={styles.itemFour}>Type</span>
+          <span className={styles.itemFive}>Customer</span>
+          <span className={styles.itemSix}>Agent</span>
+          <span className={styles.itemSeven}>Date Created</span>
+          <span className={styles.itemEight}>Details</span>
         </div> : undefined
       }
       {!loading || !pageChangeLoading ? transactions.map((transaction, index) => ( 
-        <div key={transaction.id} className={style.log}>
-          <span className={style.status}><span className={`${transaction.status === "failed" ? style.failed 
-            : transaction.status === "pending" ? style.pending : style.success}`}></span></span>            
-          <span className={style.itemTwo}>&#8358;{transaction.amount}</span>
-          <span className={style.itemThree}>{transaction.reference}</span>
-          <span className={style.itemFour}>{transaction.transtype.name}</span>
-          <span className={style.itemFive}>{transaction.customer_info}</span>
-          <span className={style.itemSix}>{transaction.agent.business_name}</span>
-          <span className={style.itemSeven}>{transaction.created_at}</span>
-          <span className={style.itemEight}>
+        <div key={transaction.id} className={styles.log}>
+          <span className={styles.status}><span className={`${transaction.status === "failed" ? styles.failed 
+            : transaction.status === "pending" ? styles.pending : styles.success}`}></span></span>            
+          <span className={styles.itemTwo}>&#8358;{transaction.amount}</span>
+          <span className={styles.itemThree}>{transaction.reference}</span>
+          <span className={styles.itemFour}>{transaction.transtype.name}</span>
+          <span className={styles.itemFive}>{transaction.customer_info}</span>
+          <span className={styles.itemSix}>{transaction.agent.business_name}</span>
+          <span className={styles.itemSeven}>{transaction.created_at}</span>
+          <span className={styles.itemEight}>
             <Link to={`/transaction-details/${transaction.reference}`}>View Details</Link>
           </span>
         </div> 
         )
       ) : undefined}
       {!loading ? 
-        <div className={style.pagination}>
+        <div className={styles.pagination}>
           <span onClick={() => {
               setPageChangeLoading(true);
               setCurrentPage(1);
             }} 
-            className={currentPage === 1 ? style.active : style.normal}>First Page</span>
+            className={currentPage === 1 ? styles.active : styles.normal}>First Page</span>
           <span onClick={() => {
               if (currentPage < lastPage) {
                 setCurrentPage(currentPage + 1);
               }
             }} 
           disabled={currentPage === lastPage}>Next Page</span>
-         {
+          <span className={styles.active} disabled>{currentPage}</span>
+         {/* {
           pageNumbers.map((page, index) => {
-            return <span key={`${index}--key`}onClick={() => {
-              setCurrentPage(page);
-              setPageChangeLoading(true);
-            }} 
-            className={currentPage === page ? style.active : style.normal}>{page}</span>
+            if (page === 1) {
+              return <span key={`${index}--key`}onClick={() => {
+                setCurrentPage(page);
+                setPageChangeLoading(true);
+              }} 
+              className={currentPage === page ? styles.active : styles.normal}>{page}</span>              
+            }
           })
-        } 
+        }  */}
           <span onClick={() => {
             if (currentPage > firstPage) {
               setCurrentPage(currentPage - 1);
@@ -129,7 +205,7 @@ export const TransactionLog = ({ changeCurrentPage, setTransactionsLog, uuid }) 
               setPageChangeLoading(true);
             }
          }} 
-        className={currentPage === lastPage ? style.active : style.normal}
+        className={currentPage === lastPage ? styles.active : styles.normal}
         disabled={currentPage === lastPage}>Last Page</span>
         </div> : undefined}
   </div>
