e275744175ab78ecd333de12c365ac96dfbf694f
olushesitoheeb <olaolushesi98@gmail.com>
2021-07-22 16:57:31 +0100

    fix - transfer

--- src/features/services/transfer/transfer-reducer.js
+++ src/features/services/transfer/transfer-reducer.js
@@ -1,21 +1,23 @@
 export const initialFormState = {
-  beneficiaryBankCode: "",
-  beneficiaryBankName: "",
-  amount: "",
-  accountNumber: "",
-  accountName: "",
-  phone: "",
-  narration: "",
-  total: "",
+    beneficiaryBankCode: '',
+    beneficiaryBankName: '',
+    amount: '',
+    accountNumber: '',
+    accountName: '',
+    phone: '',
+    narration: '',
+    total: '',
 };
 
 const FundsTransferReducer = (state, { type, payload }) => {
-  switch (type) {
-    case "UPDATE_FORM_STATE":
-      return { ...state, ...payload };
-    default:
-      return state;
-  }
+    switch (type) {
+        case 'UPDATE_FORM_STATE':
+            return { ...state, ...payload };
+        default:
+            return state;
+    }
 };
 
 export default FundsTransferReducer;
+
+// {"status":"00","message":"Account Found","data":{"name":"NOIBI KAZEEM ADEWALE","clientId":"NaN","bvn":"22166406689","account":{"id":"090110200506121759714896325490","number":"0024615656"},"status":"2","currency":"NGN","bank":"GTBank"}}
